81c5d1b6d63afb0764fe375e24cc32204bc6c391
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2024-04-25 18:10:12 +0200

    day 1

--- src/advent_of_code/day_01.clj
+++ src/advent_of_code/day_01.clj
@@ -1,11 +1,42 @@
-(ns advent-of-code.day-01)
+(ns advent-of-code.day-01
+  (:require [clojure.string :as string]))
+
+(def sample "12\n14\n1969\n100756")
+
+(defn compute-fuel
+  [mass]
+  (-> mass
+      (/ 3)
+      (int)
+      (- 2)))
+
 
 (defn part-1
   "Day 01 Part 1"
   [input]
-  input)
+  (->> input
+       string/split-lines
+       (map #(Integer/parseInt %))
+       (map compute-fuel)
+       (reduce +)))
+
+(part-1 sample)
+
+(defn compute-fuel2
+  [mass]
+  (-> mass
+      compute-fuel
+      (#(cond
+          (< % 9) %
+          :else (+ % (compute-fuel2 %))))))
 
 (defn part-2
   "Day 01 Part 2"
   [input]
-  input)
+  (->> input
+       string/split-lines
+       (map #(Integer/parseInt %))
+       (map compute-fuel2)
+       (reduce +)))
+
+(part-2 sample)
