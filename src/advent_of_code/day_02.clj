09d30c23e9fca59113eade86a15c848a41a48d4d
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2024-04-27 14:31:17 +0200

    day 2

--- src/advent_of_code/day_02.clj
+++ src/advent_of_code/day_02.clj
@@ -1,11 +1,36 @@
-(ns advent-of-code.day-02)
+(ns advent-of-code.day-02
+  (:require [clojure.string :as str]))
+
+(defn compute
+  [input]
+  (let [allValidStarts (for [i (range 0 (count input) 4)] i)]
+    (reduce
+     (fn [input i]
+       (if (= 99 (nth input i)) (reduced input)
+           (let [fun (if (= 1 (nth input i)) + *)
+                 a1 (nth input (nth input (+ i 1)))
+                 a2 (nth input (nth input (+ i 2)))
+                 pos (nth input (+ i 3))]
+             (assoc input pos (fun a1 a2)))))
+     input
+     allValidStarts)))
 
 (defn part-1
   "Day 02 Part 1"
   [input]
-  input)
+  (->> input
+       (#(str/split % #","))
+       (map #(Integer/parseInt %))
+       vec
+       (#(assoc % 1 12 2 2))
+       compute))
+
 
 (defn part-2
   "Day 02 Part 2"
   [input]
-  input)
+  (->> input
+       (#(for [x (range 0 99)
+               y (range 0 99)
+               :when (= 19690720 (part-1 (assoc % 1 x 2 y)))]
+           [x y]))))
--- test/advent_of_code/day_02_test.clj
+++ test/advent_of_code/day_02_test.clj
@@ -1,12 +1,14 @@
 (ns advent-of-code.day-02-test
-  (:require [clojure.test :refer [deftest testing is]]
+  (:require [clojure.test :refer [deftest testing is run-tests]]
             [advent-of-code.day-02 :refer [part-1 part-2]]
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
+  (let [expected 3500]
     (is (= expected (part-1 (slurp (resource "day-02-example.txt")))))))
 
 (deftest part2
   (let [expected nil]
     (is (= expected (part-2 (slurp (resource "day-02-example.txt")))))))
+
+(run-tests)
